2ee0b0b20f4920eab061c13e63e6b56863b427f8
sterf-mac <stefhwin@gmail.com>
2024-02-19 16:01:55 -0500

    ...

--- video-streaming/scripts/deploy.sh
+++ video-streaming/scripts/deploy.sh
@@ -1,5 +1,5 @@
 echo $REGISTRY_UN
 echo $REGISTRY_PW
-echo "${{ secrets.KUBE_CONFIG }}" | base64 -d > kubeconfig
+echo $secrets.KUBE_CONFIG
 
 envsubst < ./scripts/kubernetes/deploy.yaml | kubectl apply -f -
