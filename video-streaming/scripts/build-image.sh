f2a4ce9557f05ddd89c3a262ec65c8def9a97bfc
sterf-mac <stefhwin@gmail.com>
2024-02-19 15:02:53 -0500

    buildx removed

--- video-streaming/scripts/build-image.sh
+++ video-streaming/scripts/build-image.sh
@@ -1,3 +1,3 @@
 echo $CONTAINER_REGISTRY
 echo $VERSION
-docker buildx build -t $CONTAINER_REGISTRY/video-streaming:$VERSION --file ./Dockerfile-prod .
+docker build -t $CONTAINER_REGISTRY/video-streaming:$VERSION --file ./Dockerfile-prod .
